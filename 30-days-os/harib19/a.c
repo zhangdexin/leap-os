ce837823523a35dcad3e9557a3a7e43829b79d62
zhangdexin <1546711908@qq.com>
2021-06-10 09:05:35 +0800

    stack protected and kill app

---
+++ 30-days-os/harib19/a.c
@@ -0,0 +1,13 @@
+void api_putchar(int c);
+void api_end();
+
+void HariMain(void)
+{
+	api_putchar('A');
+    api_putchar('B');
+    api_putchar('C');
+    api_putchar('D');
+    api_putchar('E');
+	
+    api_end();
+}
